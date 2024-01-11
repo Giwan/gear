20e289efa48ab137fd172ccc313883c7bbf6d603
Giwan Persaud <giwan.persaud@gmail.com>
2024-01-11 09:05:01 +0100

    test: markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
     adapter: deno(),
+    integrations: [mdx()],
 });
---
+++ src/pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About Me: Giwan Persaud"
+description: "Giwan Persaud building mytoori.com"
+updatedDate: "november 08 2022"
+---
+
+[home](/)
+<hr/>
+I'm a Front-end Engineer from Amsterdam. 
